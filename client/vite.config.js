8dbbd7386a7ff88a722aae37194591c7389601bd
adunukotaashok-2007 <adunukotaashok@gmail.com>
2026-09-13 13:15:28 +0530

    Add proxy configuration for socket.io in Vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,5 +5,13 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
+    host: true,
+    proxy: {
+      "/socket.io": {
+        target: "http://localhost:3001",
+        ws: true,
+        changeOrigin: true,
+      },
+    },
   },
 });
